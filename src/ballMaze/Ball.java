1fc100fb739e94f390b70a573be9c94c3cc999ea
aaaaaalex <xli@macalester.edu>
2019-12-02 11:25:48 -0600

    Alex Li HW1

--- src/ballMaze/Ball.java
+++ src/ballMaze/Ball.java
@@ -1,22 +1,62 @@
 package ballMaze;
 
+import comp127graphics.CanvasWindow;
 import comp127graphics.Ellipse;
 
 public class Ball extends Ellipse {
+    public static final double GRAVITY = -9.8;
 
     private double x;
     private double y;
+    private double r;
     private double width;
     private double height;
+    private double dx;
+    private double dy;
+    private CanvasWindow canvasWindow;
+    private Maze maze;
+    private double boundWidth;
+    private double boundHeight;
 
-    public Ball (double x, double y, double width, double height) {
-        super(x, y, width, height);
+    public Ball(double x, double y, double r, CanvasWindow canvasWindow, Maze maze) {
+        super(x, y, 2 * r, 2 * r);
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.r = r;
+        width = 2 * r;
+        height = 2 * r;
+        this.canvasWindow = canvasWindow;
+        dx = 0.05;
+        dy = 0.05;
+        boundHeight = canvasWindow.getHeight() + maze.getHeight();
+        boundWidth = canvasWindow.getWidth() + maze.getWidth();
+        this.maze = maze;
     }
 
-//    public void
+
+    /**
+     * Updates the position of the ball if it's inside the box.
+     */
+    public void move(double dt) {
+        if (x > maze.getX() && x < maze.getX() + maze.getWidth() && y > maze.getY() && y < maze.getY() + maze.getHeight()) {
+            x = x + dx * dt;
+            y = y + dy * dt;
+            this.setCenter(x, y);
+            dy -= GRAVITY * dt;
+        }
+        System.out.println("--------ERROR: OUT OF BOUND---------");
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getX() {
+        return x + r;
+    }
+
+    public double getY() {
+        return y + r;
+    }
 
 }
