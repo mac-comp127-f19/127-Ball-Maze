be813fcd49fd29d4256a82a37c5937c4c78d4747
aaaaaalex <xli@macalester.edu>
2019-12-03 16:42:31 -0600

    Alex Li HW1

---
+++ src/ballMaze/Wall.java
@@ -0,0 +1,12 @@
+package ballMaze;
+
+import comp127graphics.CanvasWindow;
+import comp127graphics.Line;
+import comp127graphics.Rectangle;
+
+public class Wall extends Line {
+
+    public Wall(double x1, double y1, double x2, double y2) {
+        super(x1, y1, x2, y2);
+    }
+}
